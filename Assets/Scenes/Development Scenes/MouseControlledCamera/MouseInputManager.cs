13c6680a0c9298bfdb718b5d2c6b0ef3c3f72afc
Rhys Angelo-Jones <rhystaj@gmail.com>
2018-08-01 19:30:39 +1200

    Create MouseInputManager that detects mouse movement and provides a delegate to subscribe events to,

---
+++ Assets/Scenes/Development Scenes/MouseControlledCamera/MouseInputManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Detects the use of the mouse and provide delegates so that other components can react.
+/// </summary>
+public class MouseInputManager : MonoBehaviour {
+
+    public delegate void MouseMovement(float deltaX, float deltaY, float deltaZ);
+    public static MouseMovement mouseMoved;
+
+    private Vector3 previousMousePosition;
+
+    private void Start()
+    {
+        previousMousePosition = Input.mousePosition;
+    }
+
+    void Update () {
+
+        Vector3 currentMousePosition = Input.mousePosition;
+        if (!currentMousePosition.Equals(previousMousePosition))
+        {
+            if(mouseMoved != null)
+            {
+
+                float deltaX = currentMousePosition.x - previousMousePosition.x;
+                float deltaY = currentMousePosition.y - previousMousePosition.y;
+                float deltaZ = currentMousePosition.z - previousMousePosition.z;
+
+                mouseMoved(deltaX, deltaY, deltaZ);
+
+            }
+        }
+
+        previousMousePosition = currentMousePosition;
+
+	}
+}
