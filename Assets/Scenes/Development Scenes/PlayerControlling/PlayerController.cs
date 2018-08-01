ae6b96a8f67dfe7d4d60917ca558c5258fdded62
Yi Sian Lim <limyisian@gmail.com>
2018-08-02 10:54:09 +1200

    Player can now attack by clicking on right mouse

--- Assets/Scenes/Development Scenes/PlayerControlling/PlayerController.cs
+++ Assets/Scenes/Development Scenes/PlayerControlling/PlayerController.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+    // Condition = 0 for Idle.
+    // Condition = 1 for Charge.
+    // Condition = 2 for Attack. 
+    public Animator anim;
+
+    [Header("Movement")]
     public float speed;
     public float direction;
     public Rigidbody rb;
-    public Animator anim;
+
+    [Header("Combat")]
+    private bool attacking;
 
 	// Use this for initialization
 	void Start () {
@@ -21,12 +29,17 @@ public class PlayerController : MonoBehaviour {
 	}
 
     void GetInput() {
+        // Attack.
+        if (Input.GetMouseButtonDown(1)) {
+            Attack();
+        }
         // Move left.
         if (Input.GetKey(KeyCode.A)){
             SetDirection(-1);
         }
         else if (Input.GetKeyUp(KeyCode.A)) {
             SetDirection(0);
+            anim.SetInteger("Condition", 0);
         }
 
         // Move right.
@@ -37,25 +50,37 @@ public class PlayerController : MonoBehaviour {
         else if (Input.GetKeyUp(KeyCode.D))
         {
             SetDirection(0);
+            anim.SetInteger("Condition", 0);
         }
     }
 
     void Move() {
-        // If the player is not moving.
-        if (direction == 0)
-        {
-            anim.SetInteger("Condition", 0);
-            return;
+        // If the player is moving.
+        if(direction != 0) {
+            if (!attacking) {
+                anim.SetInteger("Condition", 1);
+                rb.MovePosition(transform.position + (Vector3.right * direction * speed * Time.deltaTime));
+            } 
         }
-        else {
-            anim.SetInteger("Condition", 1);
-        }
-        rb.MovePosition(transform.position + (Vector3.right * direction * speed * Time.deltaTime));
     }
 
     void SetDirection(float dir) {
         if (dir < 0) transform.LookAt(transform.position + Vector3.left);
         else if (dir > 0) transform.LookAt(transform.position + Vector3.right);
         direction = dir;
+    }
+
+    void Attack() {
+        if (attacking) return;
+        anim.SetInteger("Condition", 2);
+        StartCoroutine(AttackRoutine());
+     }
+
+    // Set attacking to true and then false, after 1 second.
+    IEnumerator AttackRoutine() {
+        attacking = true;
+        yield return new WaitForSeconds(1);
+        attacking = false;
+        anim.SetInteger("Condition", 0);
     }
 }
