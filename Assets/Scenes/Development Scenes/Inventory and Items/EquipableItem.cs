4ca8136d83a714dc517756053f9cb4ed4d285f96
Rhys Angelo-Jones <rhystaj@gmail.com>
2018-08-07 11:05:37 +1200

    Start scripts for inventory and items,

---
+++ Assets/Scenes/Development Scenes/Inventory and Items/EquipableItem.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// An item that can be pickedup and used from the inventory.
+/// </summary>
+public abstract class EquipableItem : MonoBehaviour {
+
+    /// <summary>
+    /// Activate the item.
+    /// </summary>
+    /// <param name="userTranform"> The transform of the character using the item. </param>
+    public abstract void Use(Transform userTranform);
+
+}
