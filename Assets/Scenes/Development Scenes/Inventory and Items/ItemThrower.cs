f7c0ff0addc46e0ed5aac87fcb0fa90e7ec9531c
Rhys Angelo-Jones <rhystaj@gmail.com>
2018-08-07 16:09:56 +1200

    Create an equipable item that will thow other items.
    #4

---
+++ Assets/Scenes/Development Scenes/Inventory and Items/ItemThrower.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "BombThrower", menuName = "Equipable Item/Untitled Item Thower", order = 1)]
+public class ItemThrower : EquipableItem
+{
+
+    Rigidbody item; //The type of item that is able to be thrown.
+
+    public override void Use(Transform userTranform)
+    {
+        
+    }
+}
