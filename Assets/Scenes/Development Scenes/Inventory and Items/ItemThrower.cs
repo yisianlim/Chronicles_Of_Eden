1eb558977c662d1bbaeb37652ca0b5c19201f410
Rhys Angelo-Jones <rhystaj@gmail.com>
2018-08-07 20:21:12 +1200

    Change item thrower to calculate the direction of the throw, so it can be used in the absolute coordinate system.
    #4

--- Assets/Scenes/Development Scenes/Inventory and Items/ItemThrower.cs
+++ Assets/Scenes/Development Scenes/Inventory and Items/ItemThrower.cs
@@ -7,13 +7,19 @@ public class ItemThrower : EquipableItem
 {
 
     [SerializeField] Rigidbody item; //The type of item that is able to be thrown.
-    [SerializeField] Vector3 throwForce;
+    [SerializeField] Vector2 throwForce;
+    [SerializeField] float instantiationDistanceFromPlayer;
 
     public override void Use(Transform userTranform)
     {
 
-        Rigidbody itemInstance = Instantiate(item, Vector3.zero, Quaternion.identity, userTranform);
-        itemInstance.AddForce(throwForce, ForceMode.Impulse);
+        float instantiationX = userTranform.position.x + instantiationDistanceFromPlayer * Mathf.Sin(Mathf.Deg2Rad * userTranform.eulerAngles.y);
+        float instantiationZ = userTranform.position.z + instantiationDistanceFromPlayer * Mathf.Cos(Mathf.Deg2Rad * userTranform.eulerAngles.y);
 
+        float forceX = throwForce.x * Mathf.Sin(Mathf.Deg2Rad * userTranform.eulerAngles.y);
+        float forceZ = throwForce.x * Mathf.Cos(Mathf.Deg2Rad * userTranform.eulerAngles.y);
+
+        Rigidbody instance = Instantiate(item, new Vector3(instantiationX, userTranform.position.y, instantiationZ), userTranform.rotation);
+        instance.AddForce(forceX, throwForce.y, forceZ, ForceMode.Impulse);
     }
 }
