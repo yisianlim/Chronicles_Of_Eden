31a93c003a2e3598a9661c605adf52c01ee72cb5
Rhys Angelo-Jones <rhystaj@gmail.com>
2018-08-07 19:36:55 +1200

    Create a driver for testing the item thrower item.
    #4

---
+++ Assets/Scenes/Development Scenes/Inventory and Items/TestItemThrower.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A generic driver for an item thrower.
+/// </summary>
+public class TestItemThrower : MonoBehaviour {
+
+    [SerializeField] Transform player;
+    [SerializeField] ItemThrower thower;
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetMouseButton(0))thower.Use(player);
+	}
+}
