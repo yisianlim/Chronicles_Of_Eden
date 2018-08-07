f5922152c1365b8fd213d1f9ea7b973363b757df
Rhys Angelo-Jones <rhystaj@gmail.com>
2018-08-07 17:15:55 +1200

    Create Bomb and add to test scene.

---
+++ Assets/Scenes/Development Scenes/Inventory and Items/Bomb.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A small detonatable object that immediatly begins detonation when instantiated.
+/// </summary>
+public class Bomb : Detonatable {
+
+	void Start () {
+        Detonate();
+	}
+	
+}
