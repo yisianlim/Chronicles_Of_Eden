6e0a29af5196d9cd3dd687b4ac0e7bff3b8887d4
Rhys Angelo-Jones <rhystaj@gmail.com>
2018-08-08 13:33:42 +1200

    Create abstract component which allows objects to identify nearby scannable objects, and a stub for those objects.

---
+++ Assets/Scenes/Development Scenes/Enemy AI/Scanner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// An abstract class for a Component that allows an object to identify the types of other nearby objects.
+/// </summary>
+public abstract class Scanner : MonoBehaviour {
+
+    /// <summary>
+    /// A delegate to be called when scannable objects have been identified.
+    /// </summary>
+    /// <param name="scannedObjects"></param>
+    public delegate void ScanResult(Scannable[] scannedObjects);
+    public ScanResult ObjectsScanned;
+
+    /// <summary>
+    /// Identify scannable object.
+    /// </summary>
+    /// <returns>A list of all the scannable objects nearby, null if there are none.</returns>
+    protected abstract Scannable[] Scan();
+
+    private void Update()
+    {
+        Scannable[] scannedObjects = Scan();
+        if (scannedObjects != null)
+            ObjectsScanned(scannedObjects);
+    }
+
+}
