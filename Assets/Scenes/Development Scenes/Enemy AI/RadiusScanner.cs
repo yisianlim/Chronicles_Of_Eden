4b4e1188437541599c1b1ed418655c1f85afa81c
Rhys Angelo-Jones <rhystaj@gmail.com>
2018-08-08 13:58:02 +1200

    Create a scanner that identifies objects in a sphere around the target.
    #5

---
+++ Assets/Scenes/Development Scenes/Enemy AI/RadiusScanner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A scanner that looks for scannable objects in a radius around the object.
+/// </summary>
+public class RadiusScanner : Scanner
+{
+
+    [SerializeField] float scanRadius;
+
+    protected override List<Scannable> Scan()
+    {
+
+        //Find all colliderable objects nearby.
+        Collider[] objsNearby = Physics.OverlapSphere(transform.position, scanRadius);
+
+        //Filter out the scannable objects and return them.
+        List<Scannable> scannables = new List<Scannable>();
+        new List<Collider>(objsNearby).ForEach(obj =>
+        {
+            Scannable scannable = obj.GetComponent<Scannable>();
+            if (scannable != null) scannables.Add(scannable);
+        });
+        return new List<Scannable>(scannables);
+
+    }
+}
