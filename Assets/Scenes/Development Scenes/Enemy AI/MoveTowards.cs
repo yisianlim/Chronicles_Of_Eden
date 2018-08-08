49536bb455d957400dd1e1ced8b2060d27095a80
Rhys Angelo-Jones <rhystaj@gmail.com>
2018-08-08 19:36:04 +1200

    Change movetowards so that it only follows the target along the x and z axes.
    #5

--- Assets/Scenes/Development Scenes/Enemy AI/MoveTowards.cs
+++ Assets/Scenes/Development Scenes/Enemy AI/MoveTowards.cs
@@ -12,12 +12,15 @@ public class MoveTowards : NPCBehaviour
     public override void Act(NPCAI npc, Scannable target)
     {
 
+        //Move towrds the target on the x and z axes, but not the y axis.
+        Vector3 targetPosition = new Vector3(target.transform.position.x, npc.transform.position.y, target.transform.position.z);
+
         //Move the npc a step towards the target.
-        npc.transform.position = Vector3.MoveTowards(npc.transform.position, target.transform.position, movementSpeed);
+        npc.transform.position = Vector3.MoveTowards(npc.transform.position, targetPosition, movementSpeed);
 
 
         //Turn the npc towards the target.
-        Vector3 dir = target.transform.position - npc.transform.position;
+        Vector3 dir = targetPosition - npc.transform.position;
         Vector3 rotation = Vector3.RotateTowards(npc.transform.forward, dir, turnSpeed, 0f);
         npc.transform.rotation = Quaternion.LookRotation(rotation);
 
