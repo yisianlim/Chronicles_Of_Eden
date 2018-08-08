c9a1426c9a78222888595397726faa0cf205c109
Rhys Angelo-Jones <rhystaj@gmail.com>
2018-08-08 14:44:34 +1200

    Create abstarct scriptable object to represent potential behaviours for NPCs and begin to implement AI component to use those behaviours.
    #5

---
+++ Assets/Scenes/Development Scenes/Enemy AI/NPCAI.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCAI : MonoBehaviour {
+
+    [SerializeField] Scanner scanner; //The object used to detemine other nearby objects.
+    [SerializeField] Reaction[] reactions; //The reactions of the NPC to differen object types, in order of priority.
+
+    private void OnEnable()
+    {
+        scanner.ObjectsScanned += ObjectsDetected;
+    }
+
+    private void OnDisable()
+    {
+        scanner.ObjectsScanned -= ObjectsDetected;
+    }
+
+
+    private void ObjectsDetected(Dictionary<string, List<Scannable>> detectedObjects)
+    {
+
+        foreach(Reaction reaction in reactions)
+        {
+
+            if (!detectedObjects.ContainsKey(reaction.objectType)) continue;
+        }
+
+    }
+
+    private Scannable getNearestScannable(List<Scannable>)
+    {
+        return null;
+    }
+
+    [SerializeField]
+    private class Reaction
+    {
+        public string objectType;
+        public NPCBehaviour reaction;
+    }
+
+}
---
+++ Assets/Scenes/Development Scenes/Enemy AI/NPCBehaviour.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class NPCBehaviour : ScriptableObject {
+
+    /// <summary>
+    /// Describes a frame of the behaviour.
+    /// </summary>
+    /// <param name="instigator"></param>
+    public abstract void Act(NPCAI npc, Scannable target);
+
+}
