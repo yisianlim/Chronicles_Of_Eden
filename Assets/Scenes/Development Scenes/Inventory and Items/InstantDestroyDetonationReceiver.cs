b28d0cb69d814403cda3f31a9371cb839239af4d
Rhys Angelo-Jones <rhystaj@gmail.com>
2018-08-11 10:03:35 +1200

    Make a component that is removed from the scene when it recieves a detonation.
    #13

---
+++ Assets/Scenes/Development Scenes/Inventory and Items/InstantDestroyDetonationReceiver.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantDestroyDetonationReceiver : DetonationReciever
+{
+    public override void ApplyDetonation()
+    {
+        gameObject.SetActive(false);
+    }
+}
