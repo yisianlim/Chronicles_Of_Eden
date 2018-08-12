4202a181b86979056d5b0d11906c990870667c4d
Yi Sian Lim <limyisian@gmail.com>
2018-08-12 17:25:57 +1200

    HP can now be seen at the top of the screen

--- Assets/Scripts/Stats/PlayerStat.cs
+++ Assets/Scripts/Stats/PlayerStat.cs
@@ -1,19 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerStat : MonoBehaviour {
     public int maxHealth = 500;
     public int currentHealth { get; private set; }
 
+    public Image HPImage;
+
     private void Awake()
     {
         currentHealth = maxHealth;
+        UpdateHealthBarDisplay();
+    }
+
+    private void UpdateHealthBarDisplay() {
+        float ratio = currentHealth / maxHealth;
+        HPImage.rectTransform.localScale = new Vector3(ratio, 1, 1);
     }
 
     public void TakeDamage(int damage)
     {
         currentHealth -= damage;
         Mathf.Clamp(currentHealth, 0, maxHealth);
+        UpdateHealthBarDisplay();
     }
 }
