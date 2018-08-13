1182fac2de3ed96de0bd2eb956d38b697ddbdec2
Rhys Angelo-Jones <rhystaj@gmail.com>
2018-08-14 11:36:05 +1200

    Change Compound Block to also change when the unit shape is changed.

--- Assets/Scenes/Development Scenes/Level Generation/CompundBlock.cs
+++ Assets/Scenes/Development Scenes/Level Generation/CompundBlock.cs
@@ -6,7 +6,9 @@ using UnityEditor;
 public class CompundBlock : MonoBehaviour {
 
     [SerializeField] GameObject unitShape;
-    [SerializeField] Vector3 blocks;
+    [SerializeField] Vector3 dimension;
+
+    
 
     private Vector3 prevDimensions = Vector3.zero;
     private GameObject prevUnit;
@@ -14,16 +16,16 @@ public class CompundBlock : MonoBehaviour {
     private void OnValidate()
     {
 
-        if (unitShape == null || (ReferenceEquals(prevUnit, unitShape) && blocks.Equals(prevDimensions))) return;
+        if (unitShape == null || (ReferenceEquals(prevUnit, unitShape) && dimension.Equals(prevDimensions))) return;
 
         foreach (Transform child in transform)
             child.gameObject.SetActive(false);
 
-        for(float x = transform.position.x - blocks.x / 2; x < transform.position.x + blocks.x / 2; x++)
+        for(float x = transform.position.x - dimension.x / 2; x < transform.position.x + dimension.x / 2; x++)
         {
-            for (float y = transform.position.y - blocks.y / 2; y < transform.position.y + blocks.y / 2; y++)
+            for (float y = transform.position.y - dimension.y / 2; y < transform.position.y + dimension.y / 2; y++)
             {
-                for (float z = transform.position.z - blocks.z / 2; z < transform.position.z + blocks.z / 2; z++)
+                for (float z = transform.position.z - dimension.z / 2; z < transform.position.z + dimension.z / 2; z++)
                 {
                     GameObject shapeClone = Instantiate(unitShape, transform);
                     shapeClone.transform.position = new Vector3(x, y, z);
@@ -31,9 +33,9 @@ public class CompundBlock : MonoBehaviour {
             }
         }
 
-        prevDimensions = blocks;
+        prevDimensions = dimension;
 
     }
 
-    
+
 }
