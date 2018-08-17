cddfcb42ce196570196565b15caea14c473703b2
Yi Sian Lim <limyisian@gmail.com>
2018-08-17 12:33:42 +1200

    Added switch animation and rotating platforms

--- Assets/Scenes/Development Scenes/Bomb Puzzle/Switch.cs
+++ Assets/Scenes/Development Scenes/Bomb Puzzle/Switch.cs
@@ -4,8 +4,18 @@ using UnityEngine;
 
 public class Switch : MonoBehaviour {
 
+    public Animator anim;
+    public ToggleBehaviour behaviour;
+
+    private void Awake()
+    {
+        anim.SetBool("On", false);
+    }
+
     private void OnTriggerEnter(Collider other)
     {
+        anim.SetBool("On", true);
+        behaviour.Toggle();
         Debug.Log("Solved!");
     }
 
---
+++ Assets/Scripts/Switch/RotatePlatforms.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatePlatforms : ToggleBehaviour {
+
+    public bool toggleRotate = false;
+
+    public override void Toggle()
+    {
+        toggleRotate = true;
+    }
+
+    void Update () {
+        if (toggleRotate && transform.eulerAngles.z > 270 || transform.eulerAngles.z <= 0)
+        {
+            transform.Rotate(0, 0, -1);
+        }
+	}
+}
