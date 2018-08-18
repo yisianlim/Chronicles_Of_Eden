7511136a96879a27327da34b2b9872d325f8d144
Rhys Angelo-Jones <rhystaj@gmail.com>
2018-08-18 19:35:37 +1200

    Fix FOVScanner (sort of).
    Use an inner loop inside the targets loop to account for the fact that there may be several components of a given type on a target.
    
    Remove the check that there is nothing between the origin and the target. Obviously this does not make it a true FOV, but for the purposes of detected whether a meele attack will connect it will work fine. I had trouble getting it to work, so until there is need for true FOV it is not worth the effotrt.
    
    #22

--- Assets/Scenes/Development Scenes/Enemy AI/FOVScanner.cs
+++ Assets/Scenes/Development Scenes/Enemy AI/FOVScanner.cs
@@ -14,26 +14,41 @@ public class FOVScanner : Scanner
 
         //Find all collidable objects within a given radius of the origin.
         Collider[] collidersInRadius = Physics.OverlapSphere(origin, scanRadius);
+        Debug.Log("In Radius: " + collidersInRadius.Length);
+
 
         //Find all objects within the scan angle, relative to the position the origin and the direction being faced.
         List<E> targetsInArc = new List<E>();
         foreach (Collider target in collidersInRadius) {
 
             //Check whether the object has the desired component, skip if otherwise.
-            E e = target.GetComponent<E>();
-            if (e == null) continue;
+            E[] es = target.GetComponents<E>();
+            Debug.Log("Components on " + target.gameObject.name + " :" + es.Length);
+            if (es == null) continue;
 
-            //  Find the angle to each target in radius, and consider it within FOV if the angle to it from the direction being faced
-            // is less half the given scan angle. Skip if otherwise.
-            Vector3 directionToTarget = (target.transform.position - origin).normalized;
-            float angleFromTarget = Mathf.Abs(Vector3.Angle(directionFacing, directionToTarget));
-            if (angleFromTarget > scanAngle / 2f) continue;
+            //Observe each one of the found components and add them to targets if they are reachable.
+            foreach(E e in es)
+            {
 
-            //Check that there is nothing inbewtween the target and the origin. Skip if otherwise.
-            float distanceToTarget = Vector3.Distance(origin, target.transform.position);
-            if (Physics.Raycast(origin, directionToTarget)) continue;
 
-            targetsInArc.Add(e);
+                
+                //  Find the direction to the target, and skip if it is the zero vector (i.e. target is in exactly the same place as origin, which means it
+                // can be assumed it is the object at the origin).
+                Vector3 directionToTarget = (target.transform.position - origin).normalized;
+                if (directionToTarget.Equals(Vector3.zero)) continue;
+
+                
+
+                //  Find the angle to each target in radius, and consider it within FOV if the angle to it from the direction being faced
+                // is less half the given scan angle. Skip if otherwise.
+                float angleFromTarget = Mathf.Abs(Vector3.SignedAngle(directionFacing, directionToTarget, Vector3.up));
+                if (angleFromTarget > scanAngle / 2f) continue;
+
+                //TODO: Add code that checks whether there is something blocking the FOV between origin and target.
+
+                targetsInArc.Add(e);
+
+            }
 
         }
 
