d86aa6e0d8039d2de151e8948c8fa6744c387235
Rhys Angelo-Jones <rhystaj@gmail.com>
2018-08-18 16:07:58 +1200

    Create FOVScanner Scanner with a static method for determing whether something in within the field of view.
    The core of this scanner has not been imlemented and this will probably not be used. I just needed a FOV method and this seemed like a logical place to put it, and a FOVScanner could come in handly later.
    #22

---
+++ Assets/Scenes/Development Scenes/Enemy AI/FOVScanner.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FOVScanner : Scanner
+{
+    protected override ICollection<Scannable> Scan(Transform originTranform)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public static E[] FOVScanForObjectsOfType<E>(Vector3 origin, Vector3 directionFacing, float scanRadius, float scanAngle)
+    {
+
+        //Find all collidable objects within a given radius of the origin.
+        Collider[] collidersInRadius = Physics.OverlapSphere(origin, scanRadius);
+
+        //Find all objects within the scan angle, relative to the position the origin and the direction being faced.
+        List<E> targetsInArc = new List<E>();
+        foreach (Collider target in collidersInRadius) {
+
+            //Check whether the object has the desired component, skip if otherwise.
+            E e = target.GetComponent<E>();
+            if (e == null) continue;
+
+            //  Find the angle to each target in radius, and consider it within FOV if the angle to it from the direction being faced
+            // is less half the given scan angle. Skip if otherwise.
+            Vector3 directionToTarget = (target.transform.position - origin).normalized;
+            float angleFromTarget = Mathf.Abs(Vector3.Angle(directionFacing, directionToTarget));
+            if (angleFromTarget > scanAngle / 2f) continue;
+
+            //Check that there is nothing inbewtween the target and the origin. Skip if otherwise.
+            float distanceToTarget = Vector3.Distance(origin, target.transform.position);
+            if (Physics.Raycast(origin, directionToTarget)) continue;
+
+            targetsInArc.Add(e);
+
+        }
+
+        return targetsInArc.ToArray();
+
+    }
+
+}
