2a2156f5562448744201ec173983a8af63fbbb99
Rhys Angelo-Jones <rhystaj@gmail.com>
2018-08-18 13:35:40 +1200

    Create class DamageReciever.
    This is inteded to be used to detect when something is damaged. Enemies and the player use it, but it could also be used for other things.

---
+++ Assets/Scenes/Development Scenes/Combat/DamageReciever.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class DamageReciever : MonoBehaviour {
+
+    /// <summary>
+    /// Apply the given amount of damage to the target.
+    /// </summary>
+    /// <param name="damage"></param>
+    public abstract void ApplyDamage(int damage);
+
+}
