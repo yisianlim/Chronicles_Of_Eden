f7eaf488e1abdd75ecde6c72e58cdb5cae26102e
Aliz <eiizteayzhy.px@gmail.com>
2018-08-20 10:29:20 +1200

    added UI

---
+++ Assets/Scripts/Minimap.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minimap : MonoBehaviour {
+
+    public Transform Character_Main;
+
+	private void LateUpdate()
+	{
+        Vector3 newPosition = Character_Main.position;
+        newPosition.y = transform.position.y;
+        transform.position = newPosition;
+
+        transform.rotation = Quaternion.Euler(90f, Character_Main.eulerAngles.y, 0f);
+	}
+}
