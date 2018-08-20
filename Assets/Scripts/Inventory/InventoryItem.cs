1b737352a480b224d87750d2914c85ca3ec2cd70
Yi Sian Lim <limyisian@gmail.com>
2018-08-20 13:28:58 +1200

    Inventory now reflects the weapons picked up by player

---
+++ Assets/Scripts/Inventory/InventoryItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//public interface  {
+//    string Name { get; }
+
+//    Sprite Image { get; }
+
+//    void OnPickUp();
+//}
+
+public class InventoryEventArgs : EventArgs {
+
+    public EquipableItem Item;
+
+    public InventoryEventArgs(EquipableItem item) {
+        Item = item;
+    }
+}
