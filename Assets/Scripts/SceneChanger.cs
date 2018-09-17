05a426da478a6c374c3f820eedbed3094984ab11
Yi Sian Lim <limyisian@gmail.com>
2018-09-17 12:41:39 +1200

    Added SceneChanger into prefabs which changes Scene

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour {
+
+    public string nextScene;
+
+    public void ChangeScene() {
+        SceneManager.LoadScene(nextScene);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        ChangeScene();
+        Debug.Log("New scene!");
+    }
+}
