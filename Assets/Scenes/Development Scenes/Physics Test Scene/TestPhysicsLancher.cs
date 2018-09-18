af95937b615757406e3a97def2b07139ec78ff14
Rhys Angelo-Jones <rhystaj@gmail.com>
2018-09-18 20:51:20 +1200

    Set up test scene and script for velocity physics calculation.
    #30

---
+++ Assets/Scenes/Development Scenes/Physics Test Scene/TestPhysicsLancher.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPhysicsLancher : MonoBehaviour {
+
+    [SerializeField] Transform target;
+    [SerializeField] float arcHeightDifference;
+    [SerializeField] Rigidbody launchObject;
+
+	// Use this for initialization
+	void Update () {
+
+        if (!Input.anyKeyDown) return;
+
+        Rigidbody obj = Instantiate(launchObject, transform);
+        
+        Vector3 vel = Calculations.determineRequiredLaunchVelocityToReachPoint(transform.position, target.position, arcHeightDifference);
+        Debug.Log(vel);
+        obj.velocity = vel;
+	}
+}
