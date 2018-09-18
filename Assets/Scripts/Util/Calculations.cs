57268a01d31da2bd1e13ae31bc286aaf65d0b4ad
Rhys Angelo-Jones <rhystaj@gmail.com>
2018-09-18 21:08:37 +1200

    Fix brackets in x and z velocity calculations.
    #30

--- Assets/Scripts/Util/Calculations.cs
+++ Assets/Scripts/Util/Calculations.cs
@@ -11,16 +11,18 @@ public class Calculations{
     /// <param name="endPos">The end position of the object after launch.</param>
     /// <param name="arcHeightDifferece">How much higher than the max hieght of the two positions the top of the objects arc will be.</param>
     /// <returns></returns>
-    public static Vector3 determineRequiredLaunchVelocityToReachPoint(Vector3 startPos, Vector3 endPos, float arcHeightDifferece)
+    public static Vector3 determineRequiredLaunchVelocityToReachPoint(Vector3 startPos, Vector3 endPos, float arcHeightDifference)
     {
 
         Vector3 displacement = endPos - startPos; //The distance traveled along each of the axes.
-        float arcMaxHeight = displacement.y + arcHeightDifferece;
+        float arcMaxHeight = Mathf.Abs(displacement.y) + arcHeightDifference;
+
+        Debug.Log(displacement.y + " + " + arcHeightDifference + " = " + arcMaxHeight);
 
         //Calcutale each of the velocities using Kinematics.
-        float xVelocity = displacement.x / (Mathf.Sqrt(-2 * arcMaxHeight / Physics.gravity.y) + Mathf.Sqrt(2 * displacement.y - arcMaxHeight) / Physics.gravity.y);
+        float xVelocity = displacement.x / (Mathf.Sqrt(-2 * arcMaxHeight / Physics.gravity.y) + Mathf.Sqrt(2 * (displacement.y - arcMaxHeight) / Physics.gravity.y));
         float yVelocity = Mathf.Sqrt(-2 * Physics.gravity.y * arcMaxHeight);
-        float zVelocity = displacement.z / (Mathf.Sqrt(-2 * arcMaxHeight / Physics.gravity.y) + Mathf.Sqrt(2 * displacement.y - arcMaxHeight) / Physics.gravity.y);
+        float zVelocity = displacement.z / (Mathf.Sqrt(-2 * arcMaxHeight / Physics.gravity.y) + Mathf.Sqrt(2 * (displacement.y - arcMaxHeight) / Physics.gravity.y));
 
         return new Vector3(xVelocity, yVelocity, zVelocity);
 
