e1f47b39a1adfae890f5f7cd249f7d7ba680b048
Rhys Angelo-Jones <rhystaj@gmail.com>
2018-09-24 13:31:22 +1200

    Change item thrower so that that the method that calculates where the item is thrown from can be used externally.
    #30

--- Assets/Scripts/Inventory/ItemThrower.cs
+++ Assets/Scripts/Inventory/ItemThrower.cs
@@ -13,13 +13,32 @@ public class ItemThrower : EquipableItem
     public override void Use(Transform userTranform, Inventory inventory)
     {
 
-        float instantiationX = userTranform.position.x + instantiationDistanceFromPlayer * Mathf.Sin(Mathf.Deg2Rad * userTranform.eulerAngles.y);
-        float instantiationZ = userTranform.position.z + instantiationDistanceFromPlayer * Mathf.Cos(Mathf.Deg2Rad * userTranform.eulerAngles.y);
+        Debug.Log("Item Thrower Used");
+
+        Vector3 instantationPoint = CalculateInstantiationPoint(userTranform, instantiationDistanceFromPlayer);
 
         float forceX = throwForce.x * Mathf.Sin(Mathf.Deg2Rad * userTranform.eulerAngles.y);
         float forceZ = throwForce.x * Mathf.Cos(Mathf.Deg2Rad * userTranform.eulerAngles.y);
 
-        Rigidbody instance = Instantiate(item, new Vector3(instantiationX, userTranform.position.y, instantiationZ), userTranform.rotation);
+        Rigidbody instance = Instantiate(item, new Vector3(instantationPoint.x, userTranform.position.y, instantationPoint.z), userTranform.rotation);
         instance.AddForce(forceX, throwForce.y, forceZ, ForceMode.Impulse);
+
     }
+
+    /// <summary>
+    /// Find point the given radius from the position of the given origin transform that is directly infront of the player.
+    /// </summary>
+    /// <param name="origin"></param>
+    /// <param name="instantiationRadius"></param>
+    /// <returns></returns>
+    public static Vector3 CalculateInstantiationPoint(Transform origin, float instantiationRadius)
+    {
+
+        float instantiationX = origin.position.x + instantiationRadius * Mathf.Sin(Mathf.Deg2Rad * origin.eulerAngles.y);
+        float instantiationZ = origin.position.z + instantiationRadius * Mathf.Cos(Mathf.Deg2Rad * origin.eulerAngles.y);
+
+        return new Vector3(instantiationX, origin.position.y, instantiationZ);
+
+    }
+
 }
