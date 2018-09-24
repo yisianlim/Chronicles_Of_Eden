fab78c1c067bc7aaf282d84779b90d290398c21e
Rhys Angelo-Jones <rhystaj@gmail.com>
2018-09-24 17:22:54 +1200

    Add method for calculating the displacement along a launch arc to Calculations.
    #30

--- Assets/Scripts/Util/Calculations.cs
+++ Assets/Scripts/Util/Calculations.cs
@@ -11,20 +11,59 @@ public class Calculations{
     /// <param name="endPos">The end position of the object after launch.</param>
     /// <param name="arcHeightDifferece">How much higher than the max hieght of the two positions the top of the objects arc will be.</param>
     /// <returns></returns>
-    public static Vector3 determineRequiredLaunchVelocityToReachPoint(Vector3 startPos, Vector3 endPos, float arcHeightDifference)
+    public static LaunchData DetermineRequiredLaunchToReachPoint(Vector3 startPos, Vector3 endPos, float arcHeightDifference)
     {
 
         Vector3 displacement = endPos - startPos; //The distance traveled along each of the axes.
         float arcMaxHeight = Mathf.Abs(displacement.y) + arcHeightDifference;
 
-        Debug.Log(displacement.y + " + " + arcHeightDifference + " = " + arcMaxHeight);
+        float travelTime = DetermineArcTravelTime(displacement.y, arcMaxHeight);
 
         //Calcutale each of the velocities using Kinematics.
-        float xVelocity = displacement.x / (Mathf.Sqrt(-2 * arcMaxHeight / Physics.gravity.y) + Mathf.Sqrt(2 * (displacement.y - arcMaxHeight) / Physics.gravity.y));
+        float xVelocity = displacement.x / travelTime;
         float yVelocity = Mathf.Sqrt(-2 * Physics.gravity.y * arcMaxHeight);
-        float zVelocity = displacement.z / (Mathf.Sqrt(-2 * arcMaxHeight / Physics.gravity.y) + Mathf.Sqrt(2 * (displacement.y - arcMaxHeight) / Physics.gravity.y));
+        float zVelocity = displacement.z / travelTime;
 
-        return new Vector3(xVelocity, yVelocity, zVelocity);
+        return new LaunchData(new Vector3(xVelocity, yVelocity, zVelocity), travelTime);
+
+    }
+
+    /// <summary>
+    /// Calculates the time it would take an object to travel in an arc, defined by the vertical displacement between the start and the mx height of the arc.
+    /// </summary>
+    /// <param name="yDisplacement"></param>
+    /// <param name="arcMaxHeight"></param>
+    /// <returns></returns>
+    public static float DetermineArcTravelTime(float yDisplacement, float arcMaxHeight)
+    {
+        return Mathf.Sqrt(-2 * arcMaxHeight / Physics.gravity.y) + Mathf.Sqrt(2 * (yDisplacement - arcMaxHeight) / Physics.gravity.y);
+    }
+
+    /// <summary>
+    /// Calculate the displacement along a given launch of initial velocity, at the given time.
+    /// </summary>
+    /// <param name="launchVelocity"></param>
+    /// <param name="time"></param>
+    /// <returns></returns>
+    public static Vector3 DetermineDisplacementAlongLaunchArc(Vector3 launchVelocity, float time)
+    {
+        return launchVelocity * time + Physics.gravity * time * time / 2;
+    }
+
+    /// <summary>
+    /// Represents the data for a launch.
+    /// </summary>
+    public struct LaunchData
+    {
+
+        public readonly Vector3 launchVelocity;
+        public readonly float travelTime;
+
+        public LaunchData(Vector3 launchVelocity, float travelTime)
+        {
+            this.launchVelocity = launchVelocity;
+            this.travelTime = travelTime;
+        }
 
     }
 
