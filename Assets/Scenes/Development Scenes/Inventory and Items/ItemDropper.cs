e1a9488a4c95a19d4dfe6fd1afdc7c28c83cf167
Rhys Angelo-Jones <rhystaj@gmail.com>
2018-09-25 10:47:06 +1200

    Serialise fields so they can be set in inspector.
    #36

--- Assets/Scenes/Development Scenes/Inventory and Items/ItemDropper.cs
+++ Assets/Scenes/Development Scenes/Inventory and Items/ItemDropper.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Item Dropper", menuName = "Equipable Item/Item Dropper", order = 4)]
 public class ItemDropper : EquipableItem
 {
-    Rigidbody droppedItem;
-    float instantiationDistanceFromPlayer;
+    [SerializeField] Rigidbody droppedItem;
+    [SerializeField] float instantiationDistanceFromPlayer;
 
     public override void Use(Transform userTranform, Inventory inventory)
     {
         Vector3 instantationPoint = ItemThrower.CalculateInstantiationPoint(userTranform, instantiationDistanceFromPlayer);
         Instantiate(droppedItem, instantationPoint, userTranform.rotation);
     }
+
 }
