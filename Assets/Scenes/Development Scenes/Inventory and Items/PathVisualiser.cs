744a9ffe0bc3db2f6e5de72339fa3504fe41e8a8
Rhys Angelo-Jones <rhystaj@gmail.com>
2018-09-24 16:53:21 +1200

    Create an abstract ScriptableObject than can visualise a path defined by a series of points in the scene, and an implementation that renders an object at each point.
    #30

---
+++ Assets/Scenes/Development Scenes/Inventory and Items/PathVisualiser.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// An abstract class for visualising a path described as a series of points.
+/// </summary>
+public abstract class PathVisualiser : ScriptableObject {
+
+    /// <summary>
+    /// Visulaises the path described by the point.
+    /// </summary>
+    /// <param name="path"></param>
+    public abstract void VisualisePath(Vector3[] path);
+
+    /// <summary>
+    /// Has the path visulaisation no longer be displayed.
+    /// </summary>
+    public abstract void HidePath();
+
+}
---
+++ Assets/Scenes/Development Scenes/Inventory and Items/PointsAlongPathVisualiser.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A path visualiser that draws an object at each point in the path.
+/// </summary>
+public class PointsAlongPathVisualiser : PathVisualiser
+{
+
+    private List<GameObject> representatives = new List<GameObject>();
+
+    [SerializeField] GameObject pointRepresentative; //The object that will be drawn at each point.
+
+   
+    public override void VisualisePath(Vector3[] path)
+    {
+        
+        for(int i = 0; i < path.Length; i++)
+        {
+
+            //If there are not enough representatived to draw all the points along the path, instatiate more.
+            if (representatives.Count <= i)
+                representatives.Add(Instantiate(pointRepresentative));
+
+            representatives[i].SetActive(true);
+            representatives[i].transform.position = path[i];
+
+        }
+
+    }
+
+    public override void HidePath()
+    {
+        representatives.ForEach(r => r.SetActive(false));
+    }
+
+
+}
