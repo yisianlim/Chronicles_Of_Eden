a77e4e5dedfbe0e210f9ea8e0b9d6b1f493324fc
Ben Allan <benallan@hotmail.co.nz>
2018-09-25 11:36:15 +1200

    Petrify Potion now works. Animation freezes but texture doesn't change yet. Still needs to change to only work on big enemies and be destroyed after collision. #34

--- Assets/PetrifyPotionScript.cs
+++ Assets/PetrifyPotionScript.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PetrifyPotionScript : MonoBehaviour {
 
+    public Texture stoneTexture;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -15,7 +17,12 @@ public class PetrifyPotionScript : MonoBehaviour {
 	}
 
     private void OnCollisionEnter(Collision collision) {
-        collision.gameObject.GetComponent<Rigidbody>().isKinematic = true;
+        if (collision.collider != null && collision.collider.CompareTag("Enemy")) {
+            collision.gameObject.GetComponent<NPCAI>().Petrify();
+            collision.gameObject.GetComponent<Animator>().speed = 0;
+            //collision.gameObject.GetComponent<Animator>().avatar
+        }
         
     }
+
 }
