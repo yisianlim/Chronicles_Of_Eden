4ff4b711804f992d114c5b77a01168d604217fd4
Ben Allan <benallan@hotmail.co.nz>
2018-09-24 18:01:28 +1200

    Enemies will now move towards food items rather than the player. Still need work on taking time to eat the food. #33

---
+++ Assets/EatFoodScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EatFoodScript : MonoBehaviour {
+
+    public int amount;
+
+    private void FixedUpdate() {
+        if (this.amount <=0) {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other) {
+       if (other.CompareTag("Enemy")) {
+            amount -= 1;
+        } 
+    }
+
+    private void OnTriggerStay(Collider other) {
+        if (other.CompareTag("Enemy")) {
+            amount -= 1;
+        }
+    }
+}
