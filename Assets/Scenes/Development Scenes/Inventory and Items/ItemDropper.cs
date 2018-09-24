3318b3dead326bbd6634c456846e3fbba52b27ac
Rhys Angelo-Jones <rhystaj@gmail.com>
2018-09-25 10:42:36 +1200

    Create EquipableItem that drops items where the player is standing.
    #36

---
+++ Assets/Scenes/Development Scenes/Inventory and Items/ItemDropper.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Item Dropper", menuName = "Equipable Item/Item Dropper", order = 4)]
+public class ItemDropper : EquipableItem
+{
+    Rigidbody droppedItem;
+    float instantiationDistanceFromPlayer;
+
+    public override void Use(Transform userTranform, Inventory inventory)
+    {
+        Vector3 instantationPoint = ItemThrower.CalculateInstantiationPoint(userTranform, instantiationDistanceFromPlayer);
+        Instantiate(droppedItem, instantationPoint, userTranform.rotation);
+    }
+}
