93aedda084da3067a642b3f9b8636aaa1a47b379
Rhys Angelo-Jones <rhystaj@gmail.com>
2018-09-29 11:33:34 +1200

    Create pressure plate that something to happen as long as something is on it.
    Animation is still extremely unpolished, but we're just interested in the functionality for now.
    #41

---
+++ Assets/Scripts/Switch/PressurePlate.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Collider))]
+[RequireComponent(typeof(Animator))]
+public class PressurePlate : MonoBehaviour {
+
+    private Animator animator;
+    private int numberOnPlate;
+
+    [SerializeField] ToggleBehaviour behaviour; //The behaviour the pressure plate affects.
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        Debug.Log(other.name + " entered!");
+
+        if (numberOnPlate > 0) return; //Do not react if it is already being pressed by something else.
+
+        Debug.Log("Pressing");
+
+        animator.SetBool("On", true);
+        numberOnPlate++;
+        behaviour.Toggle();
+        
+    }
+
+
+    private void OnTriggerExit(Collider other)
+    {
+
+        if (numberOnPlate > 1) return; //Ensure the is nothing on the pressure plate.
+
+        animator.SetBool("On", false);
+        numberOnPlate--;
+        behaviour.Toggle();
+
+    }
+
+}
