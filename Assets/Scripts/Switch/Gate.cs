d69fd9ee40db4ae1c7609baed722ac3e07771843
Rhys Angelo-Jones <rhystaj@gmail.com>
2018-09-29 12:31:30 +1200

    Create gate that can be open and closed by the pressure pad.
    The gate animation at the moment is VERY rough. We need to look into how to make the gate smoothly transition between opened and closed.
    #41

---
+++ Assets/Scripts/Switch/Gate.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class Gate : ToggleBehaviour {
+
+    private Animator anim;
+
+    [SerializeField] bool open;
+    [SerializeField] RuntimeAnimatorController openGateController;
+    [SerializeField] RuntimeAnimatorController closeGateContoller;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        anim.SetBool("Open", open);
+    }
+
+    public override void Toggle()
+    {
+
+        open = !open;
+        if (open) anim.runtimeAnimatorController = openGateController;
+        else anim.runtimeAnimatorController = closeGateContoller;
+
+        anim.SetBool("Open", open);
+
+    }
+
+}
