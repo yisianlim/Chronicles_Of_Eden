ca615bbf7ea7af838816c10395730fc9df70f36a
Rhys Angelo-Jones <rhystaj@gmail.com>
2018-10-04 11:17:56 +1300

    Create Idle behavior asset.
    #46

--- Assets/Scripts/AI/Behaviors/IdleBehavior.cs
+++ Assets/Scripts/AI/Behaviors/IdleBehavior.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// A behavior in which the enemy does nothing
+/// </summary>
+[CreateAssetMenu(fileName = "Idle Behaviour", menuName = "NPC AI/Behaviors/Idle", order = 3)]
 public class IdleBehavior : NPCBehaviour
 {
     public override void Act(NPCAI npc, Scannable target) {}
