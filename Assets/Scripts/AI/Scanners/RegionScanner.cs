aaf57a2dd1296f8e2a83690c74001219e597f0e0
Rhys Angelo-Jones <rhystaj@gmail.com>
2018-10-08 17:59:40 +1300

    Create Scanner that checks for Scannable within specific trigger on the map.
    #54

---
+++ Assets/Scripts/AI/Scanners/RegionScanner.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RegionScanner : Scanner
+{
+
+    [SerializeField] RegionScannerTrigger region;
+
+    protected override ICollection<Scannable> Scan(Transform originTranform)
+    {
+        return region.Query();
+    }
+}
---
+++ Assets/Scripts/AI/Scanners/RegionScannerTrigger.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class RegionScannerTrigger : MonoBehaviour {
+
+    List<Scannable> scannablesInRegion;
+
+    /// <summary>
+    /// Returns the scanables detected in the region of the given type.
+    /// </summary>
+    /// <param name="scannableType"></param>
+    /// <returns></returns>
+    public ICollection<Scannable> Query()
+    {
+        return new List<Scannable>(scannablesInRegion);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Scannable scannable = other.GetComponent<Scannable>();
+        if (scannable != null) scannablesInRegion.Add(scannable);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Scannable scannable = other.GetComponent<Scannable>();
+        if (scannable != null) scannablesInRegion.Remove(scannable);
+    }
+
+}
