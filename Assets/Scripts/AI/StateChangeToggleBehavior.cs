e5be754e7fd7e53457149afb650839e0cd589f9d
Rhys Angelo-Jones <rhystaj@gmail.com>
2018-10-08 18:00:19 +1300

    Create a tobble behavior that also changes the state of NPCs.
    #54

---
+++ Assets/Scripts/AI/StateChangeToggleBehavior.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A toggle behavior that also changes the states of the given npcs when activated.
+/// </summary>
+public class StateChangeToggleBehavior : ToggleBehaviour
+{
+
+    [SerializeField] ToggleBehaviour baseBehavior;
+    [Header("State Change")]
+    [SerializeField] MultiStateNPCAI[] AIs;
+    [SerializeField] string newState;
+
+    public override void Toggle()
+    {
+        baseBehavior.Toggle();
+        foreach (MultiStateNPCAI ai in AIs)
+            ai.State = "newState";
+    }
+}
