fe9ed7d2bd2105931489a410ed52ff2414a2a098
Rhys Angelo-Jones <rhystaj@gmail.com>
2018-10-08 18:10:29 +1300

    Arbritraty change to test bomb pickup.

--- Assets/Scripts/AI/Scanners/RegionScanner.cs
+++ Assets/Scripts/AI/Scanners/RegionScanner.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// A scanner that looks for scannables within a specific region of the map.
+/// </summary>
+[CreateAssetMenu(fileName = "Region Scanner", menuName = "NPC AI/Scanners/Region Scanner", order = 2)]
 public class RegionScanner : Scanner
 {
 
