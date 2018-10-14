52a1b7f91f6d2a875127ad26d930075437326b00
Rhys Angelo-Jones <rhystaj@gmail.com>
2018-10-14 20:43:20 +1300

    Create TargetFilter that filters out targets an NPC can not reach by taking the NavMesh.

---
+++ Assets/Scenes/Development Scenes/Enemy AI/ReachableByNavmeshAgentTargetFilter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+/// <summary>
+/// Filters out targets that can be reached by using the NavMesh.
+/// </summary>
+[CreateAssetMenu(fileName = "Reachable By Navmesh Agent Filter", menuName = "NPC AI/Target/Filter/Reachable By NavMesh Agent", order = 1)]
+public class ReachableByNavmeshAgentTargetFilter : TargetFilter
+{
+
+    const float POINT_TOLERANCE = 0.5f;
+
+    public override bool IsValidTarget(Scannable target, NPCAI npc)
+    {
+
+        //Find where the target and NPC are on the NavMesh.
+        NavMeshHit npcPosition;
+        NavMeshHit targetPosition;
+        NavMesh.SamplePosition(npc.transform.position, out npcPosition, POINT_TOLERANCE, NavMesh.AllAreas);
+        NavMesh.SamplePosition(target.transform.position, out targetPosition, POINT_TOLERANCE, NavMesh.AllAreas);
+
+        //Try to find a path between the two points and return whether is is successful.
+        return NavMesh.CalculatePath(npcPosition.position, targetPosition.position, NavMesh.AllAreas, null);
+
+    }
+}
