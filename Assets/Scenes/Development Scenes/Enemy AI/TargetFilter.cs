58daf5e9f6a41bbfcbc617a89d2ca6b6796bf1a2
Rhys Angelo-Jones <rhystaj@gmail.com>
2018-10-14 19:43:15 +1300

    Create abtract classes for the target selection criteria, and filters.
    #63

---
+++ Assets/Scenes/Development Scenes/Enemy AI/TargetFilter.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Can be applied to a target selector to filter out targets that aren't relevant.
+/// </summary>
+public abstract class TargetFilter  {
+
+    public abstract bool IsValidTarget(Scannable target);
+
+}
---
+++ Assets/Scenes/Development Scenes/Enemy AI/TargetSelector.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Defines the criteria that an NPCAI can uses to select a single target from multiple.
+/// </summary>
+public abstract class TargetSelector : ScriptableObject {
+
+    [SerializeField] TargetFilter[] filters; //Filter criteria, all of which must be met for a target to be considered valid.
+
+    /// <summary>
+    /// Filter out any invalid targets, and then, if there are any valid targets, return one based on the selection criteria.
+    /// </summary>
+    /// <param name="targets"></param>
+    /// <returns></returns>
+	public Scannable FilterAndSelect(ICollection<Scannable> targets)
+    {
+
+        //Filter targets based on given filter.
+        List<Scannable> validTargets = new List<Scannable>();
+        foreach(Scannable target in targets)
+        {
+
+            //The target must pass all filters to be considered valid.
+            bool targetValid = true;
+            foreach(TargetFilter filter in filters)
+            {
+                if (!filter.IsValidTarget(target))
+                {
+                    //As soon as a target fails a filter, there is no point in continuing.
+                    targetValid = false;
+                    break;
+                }
+            }
+
+            if (targetValid) validTargets.Add(target);
+
+        }
+
+
+        if (validTargets.Count == 0) return null;
+        else return SelectTarget(validTargets);
+
+    }
+
+    /// <summary>
+    /// Selects a valid target based on the criteria.
+    /// </summary>
+    /// <param name="targets"></param>
+    /// <returns></returns>
+    public abstract Scannable SelectTarget(ICollection<Scannable> targets);
+
+}
