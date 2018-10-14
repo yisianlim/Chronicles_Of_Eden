a399c82b806658271edf0483c77268d394156404
Rhys Angelo-Jones <rhystaj@gmail.com>
2018-10-14 20:19:59 +1300

    Make target filter extend ScriptableObject so the filters array in TargetSelectors will show in the editor.
    #63

--- Assets/Scenes/Development Scenes/Enemy AI/TargetFilter.cs
+++ Assets/Scenes/Development Scenes/Enemy AI/TargetFilter.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 /// <summary>
 /// Can be applied to a target selector to filter out targets that aren't relevant.
 /// </summary>
-public abstract class TargetFilter  {
+public abstract class TargetFilter : ScriptableObject{
 
     public abstract bool IsValidTarget(Scannable target, NPCAI npc);
 
