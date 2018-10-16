ffe0e949a4b74c8b5ae213c65ab25ced2debf38f
Rhys Angelo-Jones <rhystaj@gmail.com>
2018-10-17 10:59:44 +1300

    Start work on ZoomingFocussingCamera.
    #65

---
+++ Assets/Scenes/Development Scenes/Camera Test Scene/ZoomingFocusingCamera.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoomingFocusingCamera : FocusingCamera {
+
+    const float MAX_SCROLL_SENSITIVITY = 1000;
+
+    [SerializeField] [Range(0, MAX_SCROLL_SENSITIVITY)] float scrollSensitivity;
+    [SerializeField] float minDistance;
+    [SerializeField] float maxDistance;
+    [SerializeField] float minAngleFromPlayer;
+    [SerializeField] float maxAngleFromPlayer;
+
+    private float currentScrollPoint = 0.5f;
+
+    private void Update()
+    {
+
+        float zoomSpeed = Input.GetAxis("Mouse ScrollWheel");
+
+        currentScrollPoint = Mathf.Clamp((scrollSensitivity / MAX_SCROLL_SENSITIVITY) * zoomSpeed, 0, 1);
+        distanceFromTarget = Mathf.Lerp(minDistance, maxDistance, currentScrollPoint);
+
+    }
+
+    
+
+}
