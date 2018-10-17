cf74eb1f6d5a02a1185db3d5aec6081ae3c1c707
Rhys Angelo-Jones <rhystaj@gmail.com>
2018-10-17 19:39:59 +1300

    Sync attack impact to animation events and have the attack behavior simply specify the target of the event.
    #70

--- Assets/Scripts/AI/Behaviors/Attack.cs
+++ Assets/Scripts/AI/Behaviors/Attack.cs
@@ -6,20 +6,6 @@ using UnityEngine;
 public class Attack : NPCBehaviour
 {
 
-    [Header("Attack Stats")]
-    [SerializeField] int strength; //The damage done by the attack.
-    [SerializeField] float warmup; //The before the first attack.
-    [SerializeField] float attackRate; //The time interval (in seconds between attacks).
-    [SerializeField] float preImpactDelay; //The time after the attack starts
-    [SerializeField] float attackDuration; //The total time of the attack.
-
-    [Header("Animations Used")]
-    [SerializeField] string idleAnimationName;
-    [SerializeField] string attackAnimationName;
-
-    bool warmedUp = false; //Whether the npc has performed its first attack since it was initialsed.
-    double intervalTime; //The time since the last attack, or the behavior was initialsed.
-
     public override bool ExtraStoppingCondition
     {
         get
@@ -30,8 +16,7 @@ public class Attack : NPCBehaviour
 
     public override void Plan(NPCAI npc, Scannable target)
     {
-        warmedUp = false;
-        intervalTime = 0;
+        SetAttackTarget(npc, target.gameObject);
     }
 
     public override void Adjust(NPCAI npc, Scannable target)
@@ -41,36 +26,22 @@ public class Attack : NPCBehaviour
 
     public override void Act(NPCAI npc, Scannable target)
     {
-
-        if((!warmedUp && intervalTime >= warmup) || (warmedUp && intervalTime >= attackRate))
-        {
-            warmedUp = true;
-
-            npc.StartCoroutine(attackCooroutine(npc, target));            
-
-            intervalTime = 0;
-        }
-        
-        intervalTime += Time.deltaTime;
-    }
-
-
-    private IEnumerator attackCooroutine(NPCAI npc, Scannable target)
-    {
-
-        npc.npcAnimator.Animation = attackAnimationName;
-        yield return new WaitForSeconds(preImpactDelay);
-
-        DamageReciever[] damageRecievers = target.GetComponents<DamageReciever>();
-        new List<DamageReciever>(damageRecievers).ForEach(r => r.ApplyDamage(strength, npc.transform.position));
-
-        yield return new WaitForSeconds(attackDuration - preImpactDelay);
-        npc.npcAnimator.Animation = idleAnimationName;
-
+        //NA
     }
 
     public override void Cease(NPCAI npc, Scannable target)
     {
-        //
+        SetAttackTarget(npc, null);
+    }
+
+    private void SetAttackTarget(NPCAI npc, GameObject target)
+    {
+
+        AttackAnimationEventHandler handler = npc.GetComponent<AttackAnimationEventHandler>();
+        if (handler == null)
+            throw new System.Exception("The npc " + npc.name + " does not have an AttackAnimationEventHandler attatched.");
+
+        handler.target = target;
+
     }
 }
---
+++ Assets/Scripts/Combat/AttackAnimationEventHandler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Contains events triggered by attack animations.
+/// </summary>
+public class AttackAnimationEventHandler : MonoBehaviour {
+
+    [HideInInspector] public GameObject target;
+
+    /// <summary>
+    /// Have an attack deal damage.
+    /// </summary>
+    /// <param name="attackStrength"></param>
+    public void AttackImpact(int attackStrength)
+    {
+
+        if (target == null) return;
+
+        DamageReciever[] damageRecievers = target.GetComponents<DamageReciever>();
+        if (damageRecievers.Length == 0)
+            throw new System.Exception("Attack animation event handler had been given an object, " + target.name + ", that does not have a damage reciver attatched.");
+
+        new List<DamageReciever>(damageRecievers).ForEach(r => r.ApplyDamage(attackStrength, transform.position));
+
+    }
+
+}
