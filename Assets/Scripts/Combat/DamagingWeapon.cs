a07bf0ec487e35a687c040f6105f14fde3c9a0e7
Rhys Angelo-Jones <rhystaj@gmail.com>
2018-10-20 11:10:23 +1300

    Create a DamagingWeapon component that damages anything that gets close enough.
    #72

---
+++ Assets/Scripts/Combat/DamagingWeapon.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Damages anything with a DamageReciever that gets close enough.
+/// </summary>
+[RequireComponent(typeof(Collider))]
+public class DamagingWeapon : MonoBehaviour {
+
+    [SerializeField] int strength; //How much damage the attack deals.
+
+    public void OnTriggerEnter(Collider other)
+    {
+
+        DamageReciever[] damageRecievers = other.GetComponents<DamageReciever>();
+        new List<DamageReciever>(damageRecievers).ForEach(reciever => reciever.ApplyDamage(strength, transform.position));
+
+    }
+
+}
