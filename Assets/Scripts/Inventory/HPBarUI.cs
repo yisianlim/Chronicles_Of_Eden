2ea17b705b7e18e74ecbf78557368185f327b863
Yi Sian Lim <limyisian@gmail.com>
2018-10-31 22:32:18 +1300

    Refactored HP Bar code #80

---
+++ Assets/Scripts/Inventory/HPBarUI.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HPBarUI : MonoBehaviour {
+
+    public PlayerStat stat;
+    public Image HPImage;
+
+    private int maxHP;
+    private int currentHP;
+
+    private void Awake()
+    {
+        maxHP = stat.maxHealth;
+    }
+
+    void Update () {
+        currentHP = stat.currentHealth;
+	}
+
+    /// <summary>
+    /// Update the HP bar.
+    /// </summary>
+    private void UpdateHealthBarDisplay()
+    {
+        float ratio = (float)currentHP / (float)maxHP;
+        HPImage.rectTransform.localScale = new Vector3(ratio, 1, 1);
+    }
+}
--- Assets/Scripts/Stats/PlayerStat.cs
+++ Assets/Scripts/Stats/PlayerStat.cs
@@ -7,28 +7,14 @@ public class PlayerStat : Stat {
     public int maxHealth = 500;
     public int currentHealth { get; private set; }
 
-    public Image HPImage;
-
-    public PlayerController playerController;
-
     private void Awake()
     {
         currentHealth = maxHealth;
-        UpdateHealthBarDisplay();
-        playerController = FindObjectOfType<PlayerController>();
-    }
-
-    private void UpdateHealthBarDisplay() {
-        float ratio = (float)currentHealth / (float)maxHealth;
-        HPImage.rectTransform.localScale = new Vector3(ratio, 1, 1);
     }
 
     public override void TakeDamage(int damage, Vector3 enemyDirection)
     {
         currentHealth -= damage;
         Mathf.Clamp(currentHealth, 0, maxHealth);
-        UpdateHealthBarDisplay();
-        Debug.Log("Taking damage.");
-        
     }
 }
