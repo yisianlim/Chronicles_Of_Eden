2e4deafa4279e1f8b4b7cb8a97a07b1702dd4bfe
Rhys Angelo-Jones <rhystaj@gmail.com>
2018-11-02 12:02:07 +1300

    Get level transition on death working.
    Still working on Fade Out.
    #85

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : Agent {
+public class PlayerController : Agent
+{
 
     private Transform cam;
     private Vector2 input;
@@ -26,7 +27,7 @@ public class PlayerController : Agent {
     [Header("Camera")]
     public Transform CameraTransform;
     public FocusingCamera CameraObject;
-
+    public SceneChanger deathSceneChanger;
 
     [Header("Combat")]
     [SerializeField] int attackStrength;
@@ -45,39 +46,41 @@ public class PlayerController : Agent {
     private void Start()
     {
 
-        if(weapon != null) weapon.enabled = false; //The weapon should not do damage by default - only when the player is attacking.
+        if (weapon != null) weapon.enabled = false; //The weapon should not do damage by default - only when the player is attacking.
         stat = GetComponent<Stat>();
 
     }
 
-    void Update() {
+    void Update()
+    {
+
+        Debug.Log(name + " stat: " + stat.currentHealth);
 
         if (stat.currentHealth <= 0)
         {
             anim.SetInteger("Condition", 6);
             return;
-        } 
+        }
 
-        Debug.Log(name + " stat: " + stat.currentHealth);
-
-       
-        
         //KnockBack();
         GetInput();
         SetAnimationAndDirection();
         //RotateCamera();
     }
 
-    void GetInput() {
+    void GetInput()
+    {
         // Attack.
 
-        if (Input.GetMouseButtonDown(1)) {
+        if (Input.GetMouseButtonDown(1))
+        {
 
             //get position to look in for attack, based on ray from screen "hitting" floor layer
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
 
-            if (Physics.Raycast(ray, out hit, 100f, clickMask)) {
+            if (Physics.Raycast(ray, out hit, 100f, clickMask))
+            {
                 Vector3 v3 = hit.point;
                 v3.y = this.transform.position.y;
                 lookAtDirection = v3;
@@ -90,11 +93,17 @@ public class PlayerController : Agent {
             Attack();
             gameObject.GetComponentInChildren<TrailRenderer>().enabled = true;
 
-        } else if (Input.GetButtonDown("Dodge") && !dodgeCD) {
+        }
+        else if (Input.GetButtonDown("Dodge") && !dodgeCD)
+        {
             Dodge();
-        } else if (Input.GetButtonDown("Rotate Left")) {
+        }
+        else if (Input.GetButtonDown("Rotate Left"))
+        {
             CameraObject.RotateCamera(-90);
-        } else if (Input.GetButtonDown("Rotate Right")) {
+        }
+        else if (Input.GetButtonDown("Rotate Right"))
+        {
             CameraObject.RotateCamera(90);
         }
 
@@ -105,7 +114,8 @@ public class PlayerController : Agent {
 
         // Compute the position of the player based on camera.
 
-        if (!dodging) {
+        if (!dodging)
+        {
             moveDirection = Vector3.zero;
 
             if (input.y > 0) moveDirection += CameraTransform.forward;
@@ -116,15 +126,19 @@ public class PlayerController : Agent {
             moveDirection.y = 0f;
             SetIsMoving(moveDirection != Vector3.zero);
         }
-        
+
 
 
         // Only move the player if it is not attacking.
-        if (!attacking) {
+        if (!attacking)
+        {
             lookAtDirection = moveDirection;
-            if (dodging) {
+            if (dodging)
+            {
                 transform.position += moveDirection.normalized * (moveSpeed * dodgeSpeed) * Time.deltaTime;
-            } else {
+            }
+            else
+            {
                 transform.position += moveDirection.normalized * moveSpeed * Time.deltaTime;
             }
             //rb.MovePosition((rb.position + moveDirection) * Time.deltaTime * moveSpeed);
@@ -132,50 +146,66 @@ public class PlayerController : Agent {
         }
     }
 
-    void SetAnimationAndDirection() {
+    void SetAnimationAndDirection()
+    {
         // If the player is moving, then we want to set it to charge animation.
         // Otherwise, we set it to idle.
-        if (!attacking) {
-            if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0) {
-                if (dodging) {
+        if (!attacking)
+        {
+            if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+            {
+                if (dodging)
+                {
                     anim.SetInteger("Condition", 3);
-                } else {
+                }
+                else
+                {
                     anim.SetInteger("Condition", 1);
                 }
 
-            } else {
+            }
+            else
+            {
 
-                if (Firing) {
+                if (Firing)
+                {
                     anim.SetInteger("Condition", 4);
-                } else {
+                }
+                else
+                {
                     anim.SetInteger("Condition", 0);
                 }
-                
+
             }
         }
     }
 
-    void Attack() {
+    void Attack()
+    {
 
         if (attacking) return; //Don't do anything if already attacking.
 
         // Switch between attack animations and audio. 
-        //if (!attackSwitch) {
-            anim.SetInteger("Condition", 5);
-            //attackSwitch = !attackSwitch;
+        if (!attackSwitch)
+        {
+            anim.SetInteger("Condition", 2);
+            attackSwitch = !attackSwitch;
             FindObjectOfType<AudioManager>().Play("Attack Voice 1");
-        //} else {
-        //    anim.SetInteger("Condition", 5);
-        //    attackSwitch = !attackSwitch;
-        //    FindObjectOfType<AudioManager>().Play("Attack Voice 2");
-        //}
+        }
+        else
+        {
+            anim.SetInteger("Condition", 5);
+            attackSwitch = !attackSwitch;
+            FindObjectOfType<AudioManager>().Play("Attack Voice 2");
+        }
         attacking = true;
     }
 
     /// <summary>
     /// An animation event called when the player should start dealing damage.
     /// </summary>
-    public void ConnectStart() {
+    public void ConnectStart()
+    {
         weapon.Active = true;
         Debug.Log("AttackStart");
     }
@@ -183,7 +213,8 @@ public class PlayerController : Agent {
     /// <summary>
     /// An animation event called when the player should stop dealing damage.
     /// </summary>
-    public void ConnectEnd(){
+    public void ConnectEnd()
+    {
         gameObject.GetComponentInChildren<TrailRenderer>().enabled = false;
         weapon.Active = false;
         Debug.Log("AttackMid");
@@ -198,11 +229,12 @@ public class PlayerController : Agent {
         anim.SetInteger("Condition", 0);
         attacking = false;
         Debug.Log("AttackEnd");
-        
+
     }
 
 
-    void Dodge() {
+    void Dodge()
+    {
 
         if (dodging) return; //Don't do anything if already dodging.
 
@@ -210,7 +242,8 @@ public class PlayerController : Agent {
 
     }
 
-    IEnumerator DodgingRoutine() {
+    IEnumerator DodgingRoutine()
+    {
         dodging = true;
         dodgeCD = true;
         yield return new WaitForSeconds(DodgeDuration);
@@ -219,7 +252,8 @@ public class PlayerController : Agent {
         dodgeCD = false;
     }
 
-    public void Fire() {
+    public void Fire()
+    {
 
         if (Firing) return; //Don't do anything if already dodging.
         Debug.Log("StartCoroutine fire");
@@ -227,7 +261,8 @@ public class PlayerController : Agent {
 
     }
 
-    IEnumerator FireRoutine() {
+    IEnumerator FireRoutine()
+    {
         Firing = true;
         yield return new WaitForSeconds(FiringDuration);
         Firing = false;
@@ -236,15 +271,22 @@ public class PlayerController : Agent {
     /// <summary>
     /// Getter method is check if the player is currently dodging.
     /// </summary>
-    public bool isDodging() {
+    public bool isDodging()
+    {
         return dodging;
     }
 
     /// <summary>
     /// Getter method is check if the player is cooling down from dodging. 
     /// </summary>
-    public bool isCoolingDown() {
+    public bool isCoolingDown()
+    {
         return dodgeCD;
+    }
+
+    public void OnKill()
+    {
+        deathSceneChanger.OnFadeComplete();
     }
 
     //protected void RotateCamera() {
