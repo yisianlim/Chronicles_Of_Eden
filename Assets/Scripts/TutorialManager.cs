c8fcd4d4d95832b541c300f1ba42cb2c3c24e1a5
Yi Sian Lim <limyisian@gmail.com>
2018-11-02 10:50:39 +1300

    Interactive tutorial #80

--- Assets/Scripts/TutorialManager.cs
+++ Assets/Scripts/TutorialManager.cs
@@ -5,62 +5,119 @@ using UnityEngine.UI;
 
 public class TutorialManager : MonoBehaviour {
 
-    // TutorialManager will be a singleton instance - only one instance
-    // is needed throughout the lifetime of the game. 
-    public static TutorialManager Instance { get; set; }
+    private bool start = false;
 
-    // UI elements for TutorialManager.
-    public GameObject tutorialPanel;
-    Text tutorialText;
+    public Text nameText;
+    public Text dialogueText;
+    public Button button;
+    public Dialogue dialogue;
 
-    // Keeps track of which tutorial we are currently at. 
-    private int popUpIndex = 0;
+    public Animator animator;
+    private Queue<string> sentences;
 
-    // List of instructions. 
-    public List<string> lines = new List<string>();
-
-    private void Awake()
+    void Start()
     {
-        // Initialize the UI elements. 
-        tutorialText = tutorialPanel.transform.Find("Text").GetComponent<Text>();
+        sentences = new Queue<string>();
+    }
 
-        // Panel is shown at the start. 
-        tutorialPanel.SetActive(true);
-
-        // To ensure the the TutorialManager remains a singleton instance.
-        if (Instance != null && Instance != this)
-        {
-            Destroy(gameObject);
-        }
-        else
-        {
-            Instance = this;
+    void Update()
+    {
+        if (start == false) {
+            StartCoroutine(Begin());
         }
     }
 
-    // Update is called once per frame
-    void Update () {
+    IEnumerator Begin() {
+        start = true;
+        yield return new WaitForSeconds(2);
+        StartTutorial();
+    }
 
-        if (popUpIndex == 0)
+    void StartTutorial() {
+        animator.SetBool("IsOpen", true);
+        nameText.text = dialogue.name;
+        sentences.Clear();
+
+        // Add all dialogue into the queue. 
+        foreach (string s in dialogue.sentences)
         {
-            if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
-            {
-                popUpIndex++;
-            }
-        }
-        else if (popUpIndex == 1) {
-            if (Input.GetKeyDown(KeyCode.Space)) {
-                popUpIndex++;
-            }
+            sentences.Enqueue(s);
         }
 
-        if (popUpIndex < lines.Count)
+        // Display the first sentence.
+        string sentence = sentences.Dequeue();
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+
+    }
+
+
+    IEnumerator TypeSentence(string sentence)
+    {
+        dialogueText.text = "";
+        foreach (char letter in sentence.ToCharArray())
         {
-            tutorialText.text = lines[popUpIndex];
-            tutorialPanel.SetActive(true);
+            dialogueText.text += letter;
+            yield return null;
         }
-        else {
-            tutorialPanel.SetActive(false);
-        }
-	}
+    }
+
+    //   // TutorialManager will be a singleton instance - only one instance
+    //   // is needed throughout the lifetime of the game. 
+    //   public static TutorialManager Instance { get; set; }
+
+    //   // UI elements for TutorialManager.
+    //   public GameObject tutorialPanel;
+    //   Text tutorialText;
+
+    //   // Keeps track of which tutorial we are currently at. 
+    //   private int popUpIndex = 0;
+
+    //   // List of instructions. 
+    //   public List<string> lines = new List<string>();
+
+    //   private void Awake()
+    //   {
+    //       // Initialize the UI elements. 
+    //       tutorialText = tutorialPanel.transform.Find("Text").GetComponent<Text>();
+
+    //       // Panel is shown at the start. 
+    //       tutorialPanel.SetActive(true);
+
+    //       // To ensure the the TutorialManager remains a singleton instance.
+    //       if (Instance != null && Instance != this)
+    //       {
+    //           Destroy(gameObject);
+    //       }
+    //       else
+    //       {
+    //           Instance = this;
+    //       }
+    //   }
+
+    //   // Update is called once per frame
+    //   void Update () {
+
+    //       if (popUpIndex == 0)
+    //       {
+    //           if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+    //           {
+    //               popUpIndex++;
+    //           }
+    //       }
+    //       else if (popUpIndex == 1) {
+    //           if (Input.GetKeyDown(KeyCode.Space)) {
+    //               popUpIndex++;
+    //           }
+    //       }
+
+    //       if (popUpIndex < lines.Count)
+    //       {
+    //           tutorialText.text = lines[popUpIndex];
+    //           tutorialPanel.SetActive(true);
+    //       }
+    //       else {
+    //           tutorialPanel.SetActive(false);
+    //       }
+    //}
 }
