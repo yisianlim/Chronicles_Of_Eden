fffc829994a395115752e7bec749d623696bcf49
Guest User <Guest@wg401-f-05-06.mddn.vuw.ac.nz>
2018-11-01 13:25:26 +1300

    Added enemy detector for combat areas, also fixed combat collider not detecting. With Rhys

---
+++ Assets/Scripts/Combat/InstantDestroyDamageReciever.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// An object that is intantly destroyed weh damage is applied to it.
+/// </summary>
+public class InstantDestroyDamageReciever : DamageReciever
+{
+    public override void ApplyDamage(int damage, Vector3 fromPosition)
+    {
+        Destroy(gameObject);
+    }
+}
