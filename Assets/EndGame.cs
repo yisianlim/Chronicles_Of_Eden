042bf9c65031dafd7c50614c36d40c92db9f3610
Rhys Angelo-Jones <rhystaj@gmail.com>
2018-11-02 19:58:42 +1300

    Create trigger that ends game.

---
+++ Assets/EndGame.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndGame : MonoBehaviour {
+
+    [SerializeField] SceneChanger changer;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        changer.FadeToScene();
+    }
+
+}
