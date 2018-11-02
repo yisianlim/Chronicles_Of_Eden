f71e9d058f53becd7a9ceeb52b2b2307d0bfd418
Rhys Angelo-Jones <rhystaj@gmail.com>
2018-11-02 22:20:19 +1300

    Make music fade out when game ends.

--- Assets/EndGame.cs
+++ Assets/EndGame.cs
@@ -6,9 +6,32 @@ public class EndGame : MonoBehaviour {
 
     [SerializeField] SceneChanger changer;
 
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] float fadeDuration;
+
     private void OnTriggerEnter(Collider other)
     {
+        StartCoroutine(SoundFade());
         changer.FadeToScene();
+    }
+
+    /// <summary>
+    /// Fades out music over given duration.
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator SoundFade()
+    {
+
+        float time = 0;
+        float startVol = musicSource.volume;
+        while (time < fadeDuration)
+        {
+            time += Time.deltaTime;
+            musicSource.volume = Mathf.Lerp(startVol, 0, time / fadeDuration);
+            
+            yield return null;
+        }
+
     }
 
 }
