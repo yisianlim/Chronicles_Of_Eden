263c0886741d9a718db6fa9e2f3f1e4cb1b9a7d9
Yi Sian Lim <limyisian@gmail.com>
2018-11-02 21:42:16 +1300

    Added main menu #80

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour {
+
+    private Image exitBorder;
+    private Image startBorder;
+
+    private void Awake()
+    {
+        Transform startSelection = transform.Find("StartButton").GetChild(0);
+        startBorder = startSelection.GetComponent<Image>();
+        Transform exitSelection = transform.Find("ExitButton").GetChild(0);
+        exitBorder = exitSelection.GetComponent<Image>();
+    }
+
+    public void StartGame() {
+        startBorder.enabled = true;
+        exitBorder.enabled = false;
+        SceneManager.LoadScene("Main Level Copy");
+    }
+
+
+    public void ExitGame() {
+        startBorder.enabled = false;
+        exitBorder.enabled = true;
+        Application.Quit();
+    }
+}
